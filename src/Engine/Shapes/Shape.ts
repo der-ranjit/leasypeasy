b4a4943efc1b2f4e8c104b7a05e9475b126c6ac3
Ranjit Mevius <ranjit.mevius@outlook.com>
2019-08-18 15:51:47 +0200

    make boundary check optional

--- src/Engine/Shapes/Shape.ts
+++ src/Engine/Shapes/Shape.ts
@@ -9,6 +9,8 @@ import { Square } from "./Square";
 export type ShapeType = typeof Circle | typeof Square | typeof Rectangle;
 
 export abstract class Shape extends RenderObject {
+    public checkBoundary = true;
+
     constructor(
         public position: Point,
         renderer: Renderer,
@@ -22,13 +24,15 @@ export abstract class Shape extends RenderObject {
     public abstract centerArountPoint(point: Point): void;
 
     public move(point: Point) {
-       const newPosition =  Point.ADD(this.position, point)
-       if(newPosition.x > this.renderer.context.canvas.width 
-            || newPosition.x < 0 
-            || newPosition.y > this.renderer.context.canvas.height 
-            || newPosition.y < 0
-        ) {
-            return;
+        const newPosition = Point.ADD(this.position, point)
+        if (this.checkBoundary) {
+            if (newPosition.x > this.renderer.context.canvas.width
+                || newPosition.x < 0
+                || newPosition.y > this.renderer.context.canvas.height
+                || newPosition.y < 0
+            ) {
+                return;
+            }
         }
         this.position = newPosition;
     }
