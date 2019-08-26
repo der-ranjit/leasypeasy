66770564af9c1e5bdbf0fbe631e98c1c774e455b
Ranjit Mevius <ranjit.mevius@outlook.com>
2019-08-26 12:48:31 +0200

    create collision test script

--- src/Games/LisiCollision/LisiCollision.ts
+++ src/Games/LisiCollision/LisiCollision.ts
@@ -3,35 +3,56 @@ import { Circle } from "../../Engine/Shapes/Circle";
 import { Point } from "../../Engine/Point";
 import { Color } from "../../Engine/Color";
 import { Collision } from "../../Engine/Collision";
+import { Rectangle } from "../../Engine/Shapes/Rectangle";
 
 export const LisiCollision = (renderer: Renderer) => {
     const canvas = renderer.context.canvas;
     
     canvas.width = 800;
-    canvas.height = 500;
+    canvas.height = 650;
 
-    const circle = new Circle(new Point(100, 200), 20, renderer, Color.YELLOW);
-    const circle2 = new Circle(new Point(200, 100), 20, renderer, Color.YELLOW);
+    const circlesColor = Color.YELLOW;
+    const circle1 = new Circle(new Point(100, 100), 50, renderer, circlesColor);
+    circle1.speed = 5
+    circle1.isControlled = true;
+    const circle2 = new Circle(new Point(300, 100), 50, renderer, circlesColor);
 
-    circle.speed = 5
-    circle.isControlled = true;
+    const rectsColor = Color.GREEN;
+    const rect1 = new Rectangle(new Point(50, 300), 100, 50, renderer, rectsColor);
+    rect1.speed = 5;
+    const rect2 = new Rectangle(new Point(200, 300), 100, 50, renderer, rectsColor);
+
+    const circleRectColor = Color.MAGENTA;
+    const circle3 = new Circle(new Point(100, 500), 50, renderer, circleRectColor);
+    circle3.speed = 5
+    const rect3 = new Rectangle(new Point(250, 500), 100, 50, renderer, circleRectColor);
+
+    let controlledIndex = 0;
+    const controllables = [circle1, rect1, circle3];
 
     window.addEventListener("keydown", (event: KeyboardEvent) => {
         if (event.key === "Enter") {
-            circle.isControlled = !circle.isControlled;
-            circle2.isControlled = !circle2.isControlled;
+            controlledIndex = (controlledIndex + 1) % controllables.length;
+            controllables.forEach(contrallable => contrallable.isControlled = false);
+            controllables[controlledIndex].isControlled = true;
         }
     })
 
+    const collisionColor = Color.RED;
     Renderer.onLoopEnd$.subscribe(() => {
-        const isColliding = Collision.isColliding(circle, circle2);
-        if (isColliding) {
-            circle.setColor(Color.RED);
-            circle2.setColor(Color.RED);
-        }
-        else {
-            circle.setColor(Color.YELLOW);
-            circle2.setColor(Color.YELLOW);
-        }
+        const isCollidingCircles = Collision.isColliding(circle1, circle2);
+        let color = isCollidingCircles ? collisionColor : circlesColor;
+        circle1.setColor(color, Color.BLACK);
+        circle2.setColor(color, Color.BLACK);
+        
+        const isCollidingRectangles = Collision.isColliding(rect1, rect2);
+        color = isCollidingRectangles ? collisionColor : rectsColor;
+        rect1.setColor(color, Color.BLACK);
+        rect2.setColor(color, Color.BLACK);
+        
+        const isCollidingCircleRectangle = Collision.isColliding(circle3, rect3);
+        color = isCollidingCircleRectangle ? collisionColor : circleRectColor;
+        circle3.setColor(color, Color.BLACK);
+        rect3.setColor(color, Color.BLACK);
     });
 }
