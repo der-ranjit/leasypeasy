a435db6ee3fb3b39dd2fad4f2ec007ffe5c7993e
Ranjit Mevius <ranjit.mevius@outlook.com>
2019-08-26 13:12:53 +0200

    add utility functions

--- src/Utils.ts
+++ src/Utils.ts
@@ -25,6 +25,19 @@ export function clamp(min: number, max: number, value: number): number {
     return value;
 }
 
+export function inRange(value: number, rangeStart: number, rangeEnd: number): boolean {
+    return value >= Math.min(rangeStart, rangeEnd) &&
+           value <= Math.max(rangeStart, rangeEnd); 
+}
+
+export function rangesIntersect(rangeAstart: number, rangeAEnd: number, rangeBStart: number, rangeBEnd: number): boolean {
+    /* The first range's largest value has to be greater than the second range's smallest value and
+       the first range's smallest value hast to be smaller than the second range's largest value
+       for an intersection to occur */
+    return Math.max(rangeAstart, rangeAEnd) >= Math.min(rangeBStart, rangeBEnd) &&
+           Math.min(rangeAstart, rangeAEnd) <= Math.max(rangeBStart, rangeBEnd)
+}
+
 export function debounce(func: Function, time: number) {
     let lastCall = 0;
     return function (...args: any[]) {
