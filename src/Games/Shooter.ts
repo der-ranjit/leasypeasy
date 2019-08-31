4ec86d8c2d7c638d5815b012f338bc85ed541e44
Ranjit Mevius <ranjit.mevius@outlook.com>
2019-08-31 18:26:54 +0200

    update shooter demo

--- src/Games/Shooter.ts
+++ src/Games/Shooter.ts
@@ -1,6 +1,7 @@
 import { Color, Point, Renderer, Controls, MathUtils } from "./../Engine";
 import { Square, Circle } from "../Engine/Shapes";
 import { Vector2D } from "../Engine/Vector2D";
+import { Shape } from "../Engine/Shapes/Shape.abstract";
 
 export const Shooter = (renderer: Renderer) => {
     const canvas = renderer.context.canvas;
@@ -12,13 +13,24 @@ export const Shooter = (renderer: Renderer) => {
     const spawnerPosition = new Point(20, 20);
     const spawner = new Square(spawnerPosition, 20, renderer);
 
-    const bullets = [];
+    const bullets: Shape[] = [];
 
-    controls.onKeyDown(" ").subscribe(_ => {
+    const addBullet = () => {
         const speed = 5;
+
         const bullet = new Circle(spawnerPosition, 2, renderer);
         bullet.velocity = new Vector2D(1, 1).setLength(speed);
         bullets.push(bullet);
+        bullet.onBoundaryCollision$.subscribe(_ => addBullet());
+    }
+
+    controls.onKeyDown(" ").subscribe(_ => {
+        addBullet();
+    });
+    
+    controls.onKeyDown("r").subscribe(_ => {
+        bullets.forEach(bullet => bullet.destroy());
+        bullets.splice(0, bullets.length) ;
     });
 
 }
