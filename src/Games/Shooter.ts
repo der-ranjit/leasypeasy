80ffcf9630052f8bfd1bc1f78938a4ce7ede7fe5
Ranjit Mevius <ranjit.mevius@outlook.com>
2019-08-31 18:18:38 +0200

    add shooter example

--- src/Games/Shooter.ts
+++ src/Games/Shooter.ts
@@ -1,4 +1,6 @@
 import { Color, Point, Renderer, Controls, MathUtils } from "./../Engine";
+import { Square, Circle } from "../Engine/Shapes";
+import { Vector2D } from "../Engine/Vector2D";
 
 export const Shooter = (renderer: Renderer) => {
     const canvas = renderer.context.canvas;
@@ -6,5 +8,17 @@ export const Shooter = (renderer: Renderer) => {
     
     canvas.width = 1200;
     canvas.height = 600;
+
+    const spawnerPosition = new Point(20, 20);
+    const spawner = new Square(spawnerPosition, 20, renderer);
+
+    const bullets = [];
+
+    controls.onKeyDown(" ").subscribe(_ => {
+        const speed = 5;
+        const bullet = new Circle(spawnerPosition, 2, renderer);
+        bullet.velocity = new Vector2D(1, 1).setLength(speed);
+        bullets.push(bullet);
+    });
 
 }
