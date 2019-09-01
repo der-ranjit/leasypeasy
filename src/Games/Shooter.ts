ac4e9a52607872074764b037a4a2bdef0af5e528
Ranjit Mevius <ranjit.mevius@outlook.com>
2019-09-01 12:20:57 +0200

    refactor shooter

--- src/Games/Shooter.ts
+++ src/Games/Shooter.ts
@@ -1,104 +1,147 @@
 import { Circle } from "../Engine/Shapes";
 import { Vector2D } from "../Engine/Vector2D";
 import { Color, Controls, Point, Renderer, Collision } from "./../Engine";
+import { Subject } from "rxjs";
+
+interface PlayerControls {
+    up: string;
+    down: string;
+    left: string;
+    right: string;
+    shoot: string;
+}
+
+const defaultControls: PlayerControls = {
+    left: "ArrowLeft",
+    up: "ArrowUp",
+    right: "ArrowRight",
+    down: "ArrowDown",
+    shoot: " "
+}
+
+class Player {
+    public readonly onHit$ = new Subject<Player>();
+    public readonly opponents: Player[] = [];
+    public score = 0;
+    public circle: Circle;
+    
+    private playerRadius = 20;
+    
+    private bullets: Circle[] = [];
+    private bulletRadius = 5;
+    private bulletSpeed = 4;
+
+    private controls = Controls.getInstance();
+
+    constructor(
+        public name: string,
+        public startPosition: Point,
+        public playerColor: Color,
+        public bulletColor: Color,
+        private renderer: Renderer,
+        playerControls = defaultControls
+    ) {
+        this.circle = new Circle(startPosition, this.playerRadius, this.renderer, this.playerColor)
+        this.circle.showVelocityIndicator = true;
+        this.circle.isControlled = true; 
+
+        this.setControlScheme(playerControls);
+
+        this.renderer.onBeforeUpdate$.subscribe(_ => {
+            this.checkOppenentsHit();
+        })
+
+    }
+
+    public shoot() {
+        let bullet = this.createBullet();
+        this.bullets.push(bullet);
+    }
+
+    private createBullet() {
+        let bullet = new Circle(this.circle.position, this.bulletRadius, this.renderer, this.bulletColor);
+        bullet.velocity = Vector2D.from(this.circle.velocity).setLength(this.bulletSpeed);
+        bullet.onBoundaryCollision$.subscribe(_ => {
+            this.destroyBullet(bullet);
+        })
+        return bullet;
+    }
+
+    private destroyBullet(bullet: Circle) {
+        bullet.destroy();
+        const index = this.bullets.indexOf(bullet);
+        this.bullets.splice(index, 1);
+    }
+
+    private checkOppenentsHit() {
+        for(const opponent of this.opponents) {
+            this.checkBulletHit(opponent);
+        }
+    }
+
+    private checkBulletHit(player: Player) {
+        this.bullets.forEach(bullet => {
+            if (Collision.isColliding(bullet, player.circle)) {
+                this.onHit$.next(player);
+                this.destroyBullet(bullet);
+            }
+        });
+    }
+
+    private setControlScheme(controls: PlayerControls) {
+        this.circle.up = controls.up;
+        this.circle.down = controls.down;
+        this.circle.left = controls.left;
+        this.circle.right = controls.right;
+        this.controls.onKeyDown(controls.shoot).subscribe(_ => {
+            this.shoot();
+        });
+    }
+}
 
 export const Shooter = (renderer: Renderer) => {
     const canvas = renderer.context.canvas;
-    const controls = Controls.getInstance();
-    
     canvas.width = 1200;
     canvas.height = 600;
 
-    let lisiScore = 0;
-    let ranzScore = 0;
+    const controls = Controls.getInstance();
     
     const lisiPosition = new Point(20, canvas.height / 2);
+    const lisiControls: PlayerControls = {
+        left: "a",
+        up: "w",
+        right: "d",
+        down: "s",
+        shoot: "q"
+    };
     const ranzPosition = new Point(canvas.width - 20, canvas.height / 2);
+
+    const ranz = new Player("Ranz", ranzPosition, Color.BLACK, Color.RED, renderer);
+    const lisi = new Player("Lisi", lisiPosition, Color.MAGENTA, Color.GREEN, renderer, lisiControls);
+    ranz.opponents.push(lisi);
+    lisi.opponents.push(ranz);
     
-    const lisi = new Circle(lisiPosition, 15, renderer, Color.MAGENTA);
-    lisi.showVelocityIndicator = true;
-    lisi.isControlled = true;
-    lisi.gravityEnabled = false;
-    lisi.up = "w";
-    lisi.down = "s";
-    lisi.left = "a";
-    lisi.right = "d";
-    
-    const ranz = new Circle(ranzPosition, 15, renderer);
-    ranz.showVelocityIndicator = true;
-    ranz.isControlled = true;
-    ranz.gravityEnabled = false;
-
-    lisi.gravitationSources.push(ranz);
-    ranz.gravitationSources.push(lisi);
-
-    const lisisLittleBalls: Circle[] = [];
-    const ranzLittleBalls: Circle[] = [];
-
-    renderer.onBeforeUpdate$.subscribe(_ => {
-        lisisLittleBalls.forEach(lisiBall => {
-           if (Collision.isColliding(lisiBall, ranz)) {
-               lisiScore++;
-               lisi.radius = lisi.radius + 0.5;
-               ranz.radius = ranz.radius - 0.5;
-               destroyLittleBall(lisiBall, lisisLittleBalls);
-           }
+    const players = [ranz, lisi];
+    players.forEach(player => {
+        player.onHit$.subscribe(hitPlayer => {
+            player.score++;
+            player.circle.radius = player.circle.radius + 1;
+            hitPlayer.circle.radius = hitPlayer.circle.radius - 1;
         });
-
-        ranzLittleBalls.forEach(ranzBall => {
-            if (Collision.isColliding(ranzBall, lisi)) {
-                ranzScore++;
-                ranz.radius = ranz.radius + 0.5;
-                lisi.radius = lisi.radius - 0.5;
-                destroyLittleBall(ranzBall, ranzLittleBalls);
-            }
-         });
-
-    });
-
+    })
 
     renderer.onLoopEnd$.subscribe(_ => {
-        renderer.context.strokeText(`countLisi: ${lisiScore}`, 1100, 50);
-        renderer.context.strokeText(`countRanz: ${ranzScore}`, 1100, 100);
-        if (ranzScore === 6) {
-            renderer.pause();
-            renderer.context.strokeText(`Ranz hat große Balls`, 600, 300);
-         }
-
-         if (lisiScore === 6) {
-            renderer.pause();
-            renderer.context.strokeText(`Lisi hat große Balls`, 600, 300);
-         }
-    });
-
-    controls.onKeyDown("q").subscribe(_ => {
-        let littleBall = createShootingBall(lisi, Color.GREEN, lisisLittleBalls);
-        lisisLittleBalls.push(littleBall);
-    });
-
-    controls.onKeyDown(" ").subscribe(_ => {
-        let littleBall = createShootingBall(ranz, Color.BLUE, ranzLittleBalls);
-        ranzLittleBalls.push(littleBall);
-        
+        const winningScore = 20;
+        players.forEach((player, index) => {
+            renderer.context.strokeText(`count ${player.name}: ${player.score}`, 1100, 50 + 50 * index);
+            if (player.score === winningScore) {
+                renderer.pause();
+                renderer.context.strokeText(`${player.name} hat große Balls`, 600, 300);
+            }
+        })
     });
 
     controls.onKeyDown("p").subscribe(_ => {
         renderer.isRunning ? renderer.pause() : renderer.unpause();
     });
-
-    const destroyLittleBall = (ball: Circle, balls: Circle[]) => {
-        ball.destroy();
-        const index = balls.indexOf(ball);
-        balls.splice(index, 1);
-    }
-
-    const createShootingBall = (circle: Circle, color = Color.BLACK, balls: Circle[]) => {
-        let littleBall = new Circle(circle.position, 5, renderer, color);
-        littleBall.velocity = Vector2D.from(circle.velocity).setLength(4);
-        littleBall.onBoundaryCollision$.subscribe(_ => {
-            destroyLittleBall(littleBall, balls);
-        })
-        return littleBall;
-    }
-
 }
